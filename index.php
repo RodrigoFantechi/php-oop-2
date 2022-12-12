568a2624da25f14998d922ada0c72fb93ab6c566
RodrigoFantechi <rodrifante595@libero.it>
2022-12-12 17:36:12 +0100

    remove comment

--- index.php
+++ index.php
@@ -5,15 +5,6 @@ require __DIR__ . '/Models/Type.php';
 
 
 
-/*
-
-
-Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
-BONUS (Opzionale):
-Il cliente potrà sia comprare i prodotti come ospite, senza doversi registrarsi nello store, oppure può iscriversi e creare un account per ricevere cosi il 20% di sconto.
-Il cliente effettua il pagamento dei prodotti nel carrello con la carta di credito, che non deve essere scaduta.
- */
-
 $products = [
     new Product('pupazzo', new Category('cane'), new Type('toy'), 'https://picsum.photos/200/300', 25),
     new Product('croccantini', new Category('gatto'), new Type('food'), 'https://picsum.photos/200/300', 8),
