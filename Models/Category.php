2e440c8a76e3ecb3163b4ef69362f73fa3d960bb
RodrigoFantechi <rodrifante595@libero.it>
2022-12-13 14:20:05 +0100

    add try and catch

--- Models/Category.php
+++ Models/Category.php
@@ -1,18 +1,19 @@
 <?php
 
-class Category{
-   public $name;
+class Category
+{
+    public $name;
 
-    public function __construct(string $name) {
-
-        if (strtoupper($name) == 'GATTO'){
-            $this->name = 'Gatto <i class="fa-solid fa-cat"></i>';
-        } else if(strtoupper($name) == 'CANE') {
-            $this->name = 'Cane <i class="fa-solid fa-dog"></i>';
-        } else{
-            die('error');
+    public function __construct(string $name)
+    {
+        try {
+            if (strtoupper($name) == 'GATTO') {
+                $this->name = 'Gatto <i class="fa-solid fa-cat"></i>';
+            } else if (strtoupper($name) == 'CANE') {
+                $this->name = 'Cane <i class="fa-solid fa-dog"></i>';
+            }
+        } catch (Exception $e) {
+            echo 'la parola è diversa da gatto o cane';
         }
     }
-
-
 }
